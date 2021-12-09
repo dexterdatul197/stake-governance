e9fa2be1dbd0d5ba526732e1e295729effb7aa27
ninhhung <hung.ninh@sotatek.com>
2021-12-09 15:52:45 +0700

    governance v2

--- src/components/governance/proposal/Proposal.tsx
+++ src/components/governance/proposal/Proposal.tsx
@@ -1,12 +1,102 @@
-import classNames from "classnames/bind";
-import styles from './Proposal.module.scss'
-import React from "react";
-const cx = classNames.bind(styles)
+import classNames from 'classnames/bind';
+import styles from './Proposal.module.scss';
+import React from 'react';
+import { TablePagination } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+const cx = classNames.bind(styles);
+const paginationStyle = makeStyles(() => ({
+  toolbar: {
+    color: 'rgba(255, 255, 255, 0.6);',
+  },
+  input: {
+    '& > .MuiTablePagination-selectIcon': {
+      color: 'rgba(255, 255, 255, 0.6);',
+    },
+  },
+  actions: {
+    '& > .Mui-disabled .MuiSvgIcon-fontSizeMedium': {
+      color: 'rgba(255, 255, 255, 0.38);',
+    },
+  },
+}));
 const Proposal: React.FC = () => {
-    return (
-        <div className={cx('governance-proposal')}>
-            <div className={cx('text-header')}>Proposal</div>
-        </div>
-    )
-}
+  const paginationClasses = paginationStyle();
+  const handleChangePage = () => {};
+  return (
+    <div className={cx('governance-proposal')}>
+      <div className={cx('text-header')}>Proposal</div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, omnis.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div></div>
+              <div>Voted</div>
+          </div>
+      </div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsam maxime illum beatae vel, illo, et nobis doloremque numquam unde modi, blanditiis facere? Eaque, non voluptatem in rem possimus eius.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div className={cx('btn-upvote')}>Up Vote</div>
+              <div className={cx('btn-downvote')}>Down Vote</div>
+          </div>
+      </div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, omnis.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div></div>
+              <div>Voted</div>
+          </div>
+      </div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsam maxime illum beatae vel, illo, et nobis doloremque numquam unde modi, blanditiis facere? Eaque, non voluptatem in rem possimus eius.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div className={cx('btn-upvote')}>Up Vote</div>
+              <div className={cx('btn-downvote')}>Down Vote</div>
+          </div>
+      </div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi, omnis.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div></div>
+              <div>Voted</div>
+          </div>
+      </div>
+      <div className={cx('row-content')}>
+          <div className={cx('row-content-left')}>
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ipsam maxime illum beatae vel, illo, et nobis doloremque numquam unde modi, blanditiis facere? Eaque, non voluptatem in rem possimus eius.</div>
+              <div>1 November 29, 2021</div>
+          </div>
+          <div className={cx('row-content-btn')}>
+              <div className={cx('btn-upvote')}>Up Vote</div>
+              <div className={cx('btn-downvote')}>Down Vote</div>
+          </div>
+      </div>
+      <TablePagination
+        rowsPerPageOptions={[5, 10, 25]}
+        component="div"
+        count={10}
+        rowsPerPage={5}
+        page={1}
+        onPageChange={handleChangePage}
+        // onRowsPerPageChange={handleChangeRowsPerPage}
+        classes={paginationClasses}
+      />
+    </div>
+  );
+};
 export default Proposal;
