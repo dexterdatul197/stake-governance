1f2bde419ebb59ebe6e3ceeb8199c637754330fe
Smoky <khoi.dao@sotatek.com>
2022-01-04 11:21:33 +0700

    smoky update sc function in FE

---
+++ src/components/balances/StakeModal/isLoading/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { CircularProgress, DialogContent, DialogActions } from '@mui/material';
+
+const isLoading = () => {
+    return (
+        <React.Fragment>
+            <DialogContent></DialogContent>
+            <DialogActions></DialogActions>
+        </React.Fragment>
+    )
+}
+
+export default isLoading
