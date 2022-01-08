95339d10e6d15090b6f183031ded3a290f1ab168
khoi.dao@sotatek.com <khoi.dao@sotatek.com>
2022-01-09 00:13:25 +0700

    feat: add coinbase wallet

--- src/hooks/useInitial.ts
+++ src/hooks/useInitial.ts
@@ -1,23 +1,50 @@
 import { useDispatch } from 'react-redux';
 import { useAppSelector } from 'src/store/hooks';
 import { useEffect } from 'react';
-import { setEthereumAddress, walletsConfig } from '../components/connect-wallet/redux/wallet';
+import {
+  setEthereumAddress,
+  setWalletName,
+  walletsConfig
+} from '../components/connect-wallet/redux/wallet';
+
+const KEY_ADDRESS_COINBASE = '-walletlink:https://www.walletlink.org:Addresses';
 
 export const useInitial = () => {
   const { wallet } = useAppSelector((state: any) => ({
     wallet: state.wallet
   }));
   const dispatch = useDispatch();
+
+  const checkConnectionWalletLinkCoinbase = () => {
+    const timer = setInterval(() => {
+      const addressCoinbaseWallet = localStorage.getItem(KEY_ADDRESS_COINBASE);
+      if (!addressCoinbaseWallet) {
+        clearInterval(timer);
+        localStorage.clear();
+        dispatch(setEthereumAddress(''));
+        dispatch(setWalletName(''));
+      }
+    }, 3000);
+  };
+
   useEffect(() => {
     const infoConnectWallet = localStorage.getItem('walletconnect');
+    const addressCoinbaseWallet = localStorage.getItem(KEY_ADDRESS_COINBASE);
+
+    // wallet-connect
     if (wallet.walletName === walletsConfig[1] && infoConnectWallet) {
       const { chainId, connected, accounts } = JSON.parse(infoConnectWallet);
       if (connected) {
         dispatch(setEthereumAddress(accounts[0]));
-        localStorage.setItem('ethereumAddress', accounts[0]);
       } else {
         localStorage.removeItem('walletconnect');
       }
+    }
+
+    // wallet-link coinbase
+    if (wallet.walletName === walletsConfig[3] && addressCoinbaseWallet) {
+      checkConnectionWalletLinkCoinbase();
+      dispatch(setEthereumAddress(addressCoinbaseWallet as string));
     }
   }, [wallet.walletName]);
   return undefined;
