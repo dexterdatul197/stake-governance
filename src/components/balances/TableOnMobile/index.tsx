77df71def8318ab594642c71af3501a36595a4d5
Sotatek-KhoiDao <khoi.dao@sotatek.com>
2022-01-08 11:48:15 +0700

    feat: infinite scroll and fetch data to history staking on mobile

--- src/components/balances/TableOnMobile/index.tsx
+++ src/components/balances/TableOnMobile/index.tsx
@@ -1,49 +1,100 @@
+import Item from 'antd/lib/list/Item';
 import classNames from 'classnames/bind';
-import { FC } from 'react';
+import { ethers } from 'ethers';
+import { FC, useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getTransactionHistory } from 'src/apis/apis';
+import { FORMAT_DATE } from 'src/constant/constants';
+import eventBus from 'src/event/event-bus';
+import { sleep } from 'src/helpers/sleep';
+import { SocketEvent } from 'src/socket/SocketEvent';
+import { useAppSelector } from 'src/store/hooks';
+import { Card } from './card';
 import styles from './styles.module.scss';
+import { ITransaction } from './transaction.slice';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 const cx = classNames.bind(styles);
 
 const CardComponent: FC = () => {
-  const Card = () => {
-    return (
-      <div className={cx('card')}>
-        <div className={cx('card-items')}>
-          <div>1</div>
-          <div className={cx('card-head-item')}>0xxxxakhfkdhfwkh...</div>
-        </div>
-        <div className={cx('card-items')}>
-          <div>Type</div>
-          <div className={cx('btn-stake')}>Stake</div>
-        </div>
-        <div className={cx('card-items')}>
-          <div>Amount</div>
-          <div className={cx('card-items-2')}>489$</div>
-        </div>
-        <div className={cx('card-items')}>
-          <div>Date</div>
-          <div className={cx('card-items-2')}>22-11-2021</div>
-        </div>
-        <div className={cx('card-items')}>
-          <div>Status</div>
-          <div className={cx('card-items-2')} style={{ color: 'rgba(16, 125, 239, 1)' }}>
-            Pending
-          </div>
-        </div>
-      </div>
-    );
+  const wallet = useAppSelector((state) => state.wallet);
+  const dispatch = useDispatch();
+  const [limit, setLimit] = useState(20);
+  const transactionData = useAppSelector((state) => state.transactions.transactions);
+  const [hasMore, setHasMore] = useState(true);
+  const getUserAddress = () => {
+    if (wallet.ethereumAddress) {
+      return wallet.ethereumAddress;
+    }
   };
+
+  const [filter, setFilter] = useState({
+    page: 1,
+    limit: limit,
+    address: getUserAddress(),
+    totalPage: 1
+  });
+
+  useEffect(() => {
+    setFilter({
+      page: 1,
+      limit: limit,
+      address: getUserAddress(),
+      totalPage: 1
+    });
+  }, [limit]);
+
+  useEffect(() => {
+    dispatch(getTransactionHistory(filter));
+    eventBus.on(SocketEvent.transactionUpdated, async () => {
+      await sleep(1000);
+    });
+  }, []);
+  useEffect(() => {
+    dispatch(getTransactionHistory(filter));
+  }, [filter]);
+
+  const fetchData = () => {
+    setTimeout(() => setLimit(limit + 20), 2000);
+  };
+  useEffect(() => {
+    if (transactionData.data.length > 0) {
+      if (transactionData.metadata.totalItem === transactionData.data.length) {
+        setHasMore(false);
+      }
+      console.log(transactionData);
+    }
+  }, [transactionData.data.length]);
   return (
     <>
       <div className={cx('card-container')}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <InfiniteScroll
+          dataLength={transactionData.data.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: 'center' }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+          // below props only if you need pull down functionality
+          refreshFunction={() => {}}
+          pullDownToRefresh
+          pullDownToRefreshThreshold={50}
+          pullDownToRefreshContent={
+            <h3 style={{ textAlign: 'center' }}>&#8595; Pull down to refresh</h3>
+          }
+          releaseToRefreshContent={
+            <h3 style={{ textAlign: 'center' }}>&#8593; Release to refresh</h3>
+          }
+        >
+          {transactionData.data.map((item: ITransaction, index: number) => (
+            <Card transaction={item} key={item.id} />
+          ))}
+        </InfiniteScroll>
       </div>
     </>
   );
 };
-
 export default CardComponent;
