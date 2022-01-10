dd1df1413d1f564ce0528990c331b554acadba7d
ninhhung <hung.ninh@sotatek.com>
2022-01-11 00:36:31 +0700

    governance rank

--- src/apis/apis.ts
+++ src/apis/apis.ts
@@ -3,18 +3,16 @@ import axiosInstance from '../config/config';
 import { Filter } from '../interfaces/SFormData';
 import { qsStringify } from './query-string';
 
+const options = {
+  baseUrl: process.env.REACT_APP_BACKEND
+};
+
 export const getProposalList = createAsyncThunk('proposal/get-all', async (body: Filter) => {
-  const options = {
-    baseUrl: process.env.REACT_APP_BACKEND
-  };
   const res = await axiosInstance(options).get(`/proposal${qsStringify(body)}`);
   return res.data;
 });
 
 export const getTransactionHistory = createAsyncThunk('/staking/history', async (body: Filter) => {
-  const options = {
-    baseUrl: process.env.REACT_APP_BACKEND
-  };
   const res = await axiosInstance(options).get(`/staking/history`, {
     params: body
   });
@@ -22,17 +20,16 @@ export const getTransactionHistory = createAsyncThunk('/staking/history', async 
 });
 
 export const getProposalDetail = async (id: number) => {
-  const options = {
-    baseUrl: process.env.REACT_APP_BACKEND
-  };
   const res = await axiosInstance(options).get(`/proposal/${id}`);
   return res.data;
 };
 
 export const getTVLData = async (body: Filter) => {
-  const options = {
-    baseUrl: process.env.REACT_APP_BACKEND
-  };
   const res = await axiosInstance(options).get(`/staking/tvl${qsStringify(body)}`);
   return res.data.data;
 };
+
+export const getRank = async (id: string) => {
+  const res = await axiosInstance(options).get(`/voter/rank/${id}`);
+  return res.data;
+}
