c608b480d0014c36bf93299b66ab9ef43e403bd9
Smoky <khoi.dao@sotatek.com>
2022-01-15 00:09:31 +0700

    filter chnStake === 0

--- src/components/governance/leaderboard/LeaderBoard.tsx
+++ src/components/governance/leaderboard/LeaderBoard.tsx
@@ -101,7 +101,7 @@ const LeaderBoard: React.FC = () => {
                           new BigNumber(b.chnStake).minus(new BigNumber(a.chnStake)).toNumber()
                         )
                         .filter((item: any) => {
-                          return item.chnStake !== 0;
+                          return Number(item.chnStake) !== 0
                         })
                         .map((item, index) => {
                           const { id, address, voteWeight, proposalsVoted, chnStake } = item;
