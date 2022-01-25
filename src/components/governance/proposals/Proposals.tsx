72cc2b93b88516a89c7d0719a4cd893d6a656e38
ninhhung <hung.ninh@sotatek.com>
2022-01-25 16:15:18 +0700

    fixbug paging, leaderboard detail

--- src/components/governance/proposals/Proposals.tsx
+++ src/components/governance/proposals/Proposals.tsx
@@ -6,6 +6,7 @@ import { useDispatch } from 'react-redux';
 import { getProposalList } from '../../../apis/apis';
 import eventBus from '../../../event/event-bus';
 import { sleep } from '../../../helpers/sleep';
+import useIsMobile from '../../../hooks/useMobile';
 import { Filter } from '../../../interfaces/SFormData';
 import { SocketEvent } from '../../../socket/SocketEvent';
 import { useAppSelector } from '../../../store/hooks';
@@ -15,7 +16,26 @@ import styles from './Proposals.module.scss';
 const cx = classNames.bind(styles);
 const paginationStyle = makeStyles(() => ({
   toolbar: {
-    color: 'var(--pagination-color)'
+    color: 'var(--pagination-color)',
+    position: 'absolute',
+    bottom: '10px',
+    right: '10px'
+  },
+  input: {
+    '& > .MuiTablePagination-selectIcon': {
+      color: 'var(--pagination-action-color)'
+    }
+  },
+  actions: {
+    '& > .Mui-disabled .MuiSvgIcon-fontSizeMedium': {
+      color: 'var(--pagination-action-color)'
+    }
+  }
+}));
+
+const mobilePaginationStyle = makeStyles(() => ({
+  toolbar: {
+    color: 'var(--pagination-color)',
   },
   input: {
     '& > .MuiTablePagination-selectIcon': {
@@ -32,6 +52,7 @@ const paginationStyle = makeStyles(() => ({
 const Proposals: React.FC = () => {
   const dispatch = useDispatch();
   const paginationClasses = paginationStyle();
+  const mobilePaginationClasses = mobilePaginationStyle();
   const currentAccount = useAppSelector((state) => state.wallet);
   const proposals = useAppSelector((state) => state.proposals.proposals);
   const [rowPerPage, setRowPerPage] = useState(5);
@@ -40,6 +61,7 @@ const Proposals: React.FC = () => {
     page: 1,
     limit: 5
   });
+  const isMobile = useIsMobile(576);
 
   const handleChangePage = (event: unknown, newPage: number) => {
     setCurrentPage(newPage);
@@ -76,7 +98,7 @@ const Proposals: React.FC = () => {
         rowsPerPage={rowPerPage}
         page={currentPage}
         onPageChange={handleChangePage}
-        classes={paginationClasses}
+        classes={!isMobile ? paginationClasses : mobilePaginationClasses}
       />
     </div>
   );
