0e4678cf317a7a453e77bc50f5519008b0f95203
smoky <khoi.dao@sotatek.com>
2022-04-20 18:26:24 +0700

    update ui for wallet

--- src/connectors/walletconnectConnector.ts
+++ src/connectors/walletconnectConnector.ts
@@ -4,9 +4,5 @@ export const walletconnect = new WalletConnectConnector({
   supportedChainIds: [parseInt(process.env.REACT_APP_CHAIN_ID as string)],
   infuraId: process.env.REACT_APP_INFURA_ID,
   bridge: 'https://uniswap.bridge.walletconnect.org',
-  qrcode: true,
-  qrcodeModalOptions: {
-    // mobileLinks: ['rainbow', 'metamask', 'argent', 'trust', 'imtoken', 'pillar'],
-    // desktopLinks: ['encrypted ink']
-  },
+  qrcode: true
 });
